668c8a3a7fc726bd8c05c5bee86c82ee017d044d
MakeTSGreatAgain <andrii.mus00@gmail.com>
2022-01-18 11:23:35 +0200

    вв

--- lesson1/task1/index.js
+++ lesson1/task1/index.js
@@ -1,2 +1,11 @@
-const rootElem = document.querySelector('#root');
-const greetingElm = React.createElement('div', { className: 'greeting' }, 'Hello, React!');
+const rootElement = document.querySelector('#root');
+
+const renderGreeting = elem => {
+  const element = React.createElement('div', { className: 'greeting' }, [
+    React.createElement('div', { className: 'greeting__title' }, 'Hello, world!'),
+    React.createElement('div', { className: 'greeting__text' }, "I'm learning React"),
+  ]);
+  ReactDOM.render(element, elem);
+};
+
+renderGreeting(rootElement);
